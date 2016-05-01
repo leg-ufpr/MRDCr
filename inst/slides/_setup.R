197727756971cc892acf5e5e0244da450de77b98
Eduardo Junior <edujrrib@gmail.com>
2016-05-01 00:24:55 -0300

    Adiciona arquivo de configurações do knitr para os slides

---
+++ inst/slides/_setup.R
@@ -0,0 +1,11 @@
+library(knitr)
+opts_chunk$set(
+    echo = FALSE,
+    cache = FALSE,
+    tidy = FALSE,
+    fig.width = 8,
+    fig.height = 5,
+    fig.align = "center",
+    out.width = "0.8\\textwidth",
+    dev.args = list(family = "Palatino"))
+
