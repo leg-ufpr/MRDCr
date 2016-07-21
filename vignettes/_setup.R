7bc3f49c1bbfc0150e5acb0d69cb8fb54cbde98a
Walmes Zeviani <walmes@ufpr.br>
2016-07-21 20:38:26 -0300

    Corrige problema do tamanho da fonte no axis da lattice.

--- vignettes/_setup.R
+++ vignettes/_setup.R
@@ -25,6 +25,7 @@ ps <- list(box.rectangle = list(col = 1, fill = c("gray70")),
            superpose.line = list(col = mycol, lty = 1),
            superpose.symbol = list(col = mycol, pch = 1),
            superpose.polygon = list(col = mycol),
-           strip.background = list(col = c("gray80", "gray50")))
+           strip.background = list(col = c("gray80", "gray50")),
+           axis.text = list(cex = 0.8))
 trellis.par.set(ps)
 # show.settings()
